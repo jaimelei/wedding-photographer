f33db73fd360004f027f15704f1636bcbbea8ff4
Jaime Lei Indicke <jaimeindick@gmail.com>
2025-08-30 15:15:18 +0800

    001 | Frontend | Init & Feature - Created the hero section.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import HeroSection from './components/sections/hero-section';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative">
+      {/* <Navbar /> */}
+      
+      {/* LANDING PAGE SECTION */}
+      <section id="home">
+        <HeroSection />
+      </section>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter_regular: ["Inter-Regular", "sans-serif"],
+        inter_bold: ["Inter-Bold", "sans-serif"],
+        merriweather_regular: ["Merriweather-Regular", "sans-serif"],
+        merriweather_bold: ["Merriweather-Bold", "sans-serif"],
+      },
+      backgroundImage: {
+        'hero': "url('../public/assets/images/hero-section.png')",
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+};
